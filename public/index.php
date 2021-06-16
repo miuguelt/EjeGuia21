078773f5cbd0e8a1c7a9c54248a931b4e600ec13
DESKTOP-BO3VUMG\Tejedor <miuguelt@gmail.com>
2021-06-15 19:29:51 -0500

    no message

--- public/index.php
+++ public/index.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
-  echo "Hola Mundo";
-  include_once("vista/formulairocliente.php");
+  include_once("/vista/formulariocliente.php");
 ?>
-</body>
-</html>
